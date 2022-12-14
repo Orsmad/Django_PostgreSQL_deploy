7529d1c3fa07a7a88991fcf0653aefcd2bdb64a3
orsmad <smadgaor@gmail.com>
2022-12-14 23:09:42 +0200

    anCorsFix

--- api/models.py
+++ api/models.py
@@ -3,12 +3,12 @@ import uuid
 
 """Note model"""
 class Note(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=40,editable=True)
-    text = models.TextField(max_length=450,null=True, blank=True,editable=True)
-    pub_date = models.DateTimeField(auto_now_add=True, editable=False)
-    upd_date = models.DateTimeField(auto_now=True)
-    completed= models.BooleanField(default=False,null=True, blank=True,editable=True)
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title           = models.CharField(max_length=40,editable=True)
+    text            = models.TextField(max_length=450,null=True, blank=True,editable=True)
+    pub_date        = models.DateTimeField(auto_now_add=True, editable=False)
+    upd_date        = models.DateTimeField(auto_now=True)
+    completed       = models.BooleanField(default=False,null=True, blank=True,editable=True)
 
     def __str__(self) -> str:
         return self.title

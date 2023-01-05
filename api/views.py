448f37892c884b3915089961c86409df9cc19f79
orsmad <smadgaor@gmail.com>
2023-01-05 11:36:56 +0200

    qaconsid

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
@@ -13,7 +16,7 @@ from .models import Note
 def apiOverview(request):
 	api_urls = {
 		'notes':'/notes/',
-		'note':'/notes/<str:pk>/',
+		'notes':'/notes/<str:pk>/',
 		'help': 'GET,POST,DELETE,PUT'
 		}
 
@@ -24,37 +27,48 @@ def apiOverview(request):
 def notes(request,pk=-1):
     # get all or by id route
     if request.method == "GET":
-        try:
-            notes = Note.objects.get(id=pk)
-            serializer = TaskSerializer(notes, many=False)
-            return Response(serializer.data)
-        except:
+        if pk ==-1:
             notes = Note.objects.all().order_by('-id')
             serializer = TaskSerializer(notes, many=True)
             return Response(serializer.data)
-
+        else:
+            try:
+                notes = Note.objects.get(id=pk)
+                serializer = TaskSerializer(notes, many=False)
+                return Response(serializer.data)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+          
     # post method
     elif request.method == "POST":
         serializer = TaskSerializer(data=request.data)
         print(request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # update obj by id
     elif request.method == "PUT":
-        note = Note.objects.get(id=pk)
-        serializer = TaskSerializer(instance=note, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
-   
+        try:
+            note = Note.objects.get(id=pk)
+            serializer = TaskSerializer(instance=note, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
     # delede obj by id
     elif request.method == "DELETE":
         try:
             note = Note.objects.get(id=pk)
             note.delete()
-        except:
-            return Response('Item was not found!')
-        return Response('Item succsesfully delete!')
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except :
+                return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
